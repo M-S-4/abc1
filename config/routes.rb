d9f60911ddc1c76eb3e249ef5f8c585631e7d052
M-S-4 <martin.sonnino@hotmail.fr>
2016-08-22 14:39:42 +0200

    Gemfile and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
  Rails.application.routes.draw do
 
   root 'pages#index'
-  get 'about' => 'pages#about'
-  get 'courses' => 'courses#index'
-  get 'courses/:slug' => 'courses#show', as: 'course'
-  get 'courses/:slug_course/episodes/:slug_episode' => 'episodes#show', as: 'episode'
+  get '/about' => 'pages#about'
+  get '/courses' => 'courses#index'
+  get '/courses/:slug' => 'courses#show', as: 'course'
+  get '/courses/:slug_course/episodes/:slug_episode' => 'episodes#show', as: 'episode'
 
   devise_for :users
 
